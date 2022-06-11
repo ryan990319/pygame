36e93b7c2873238cfecdc6d0cf7f3f29740a7d2b
ryan990319 <ryancwego@gmail.com>
2022-06-11 17:00:13 +0800

    import pygame pygame.init()#程式初始化 screen = pygame.display.set_mode((500,600))#螢幕顯示 running = True while running:     for event in pygame.event.get():#取得玩家輸入         if event.type == pygame.QUIT:             running = False#跳出迴圈
    pygame.quit()

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import pygame
-print("Hello pygame")
-print("hello")
+pygame.init()#程式初始化
+screen = pygame.display.set_mode((500,600))#螢幕顯示
+running = True
+while running:
+    for event in pygame.event.get():#取得玩家輸入
+        if event.type == pygame.QUIT:
+            running = False#跳出迴圈
+
+pygame.quit()
